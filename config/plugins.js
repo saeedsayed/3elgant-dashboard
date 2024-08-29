454810e3d533335de5f9ff3df6010cecf1c9ee7e
saeed pc <saeidsaued@gmail.com>
2024-08-29 13:46:41 +0300

    remove vercel plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,4 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
-    enabled: true,
-    config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
-  },
   upload: {
     config: {
       provider: 'cloudinary',
